dd66987f900022df99a0f957c06d6ccdb1863213
Munif Tanjim <hello@muniftanjim.dev>
2026-08-06 01:42:26 +0600

    test(e2elive): add a docker container helper and build the test image in TestMain

--- internal/e2elive/main_test.go
+++ internal/e2elive/main_test.go
@@ -4,8 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"os/exec"
-	"path/filepath"
 	"testing"
 )
 
@@ -14,22 +12,12 @@ func TestMain(m *testing.M) {
 	if testing.Short() {
 		os.Exit(m.Run())
 	}
-	dir, err := os.MkdirTemp("", "argusbin")
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "e2elive: mktemp:", err)
+	sweepStale()
+	if err := buildTestImage(); err != nil {
+		fmt.Fprintln(os.Stderr, "e2elive:", err)
 		os.Exit(1)
 	}
-	bin := filepath.Join(dir, "argus")
-	build := exec.Command("go", "build", "-o", bin, "github.com/MunifTanjim/argus/cmd/argus")
-	build.Stdout = os.Stderr
-	build.Stderr = os.Stderr
-	if err := build.Run(); err != nil {
-		fmt.Fprintln(os.Stderr, "e2elive: build argus:", err)
-		os.RemoveAll(dir)
-		os.Exit(1)
-	}
-	argusBin = bin
 	code := m.Run()
-	os.RemoveAll(dir)
+	sweepStale()
 	os.Exit(code)
 }
