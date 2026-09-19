72372ca89726da02c7571aed0d75e9f174f88a7b
Munif Tanjim <hello@muniftanjim.dev>
2026-09-19 14:06:26 +0600

    test(app): add dismiss override to FakeSessionRepository

--- app/test/support/fake_session_repository.dart
+++ app/test/support/fake_session_repository.dart
@@ -45,6 +45,9 @@ class FakeSessionRepository implements SessionRepository {
   Future<Result<void>> kill(String sessionId) async => const Result.ok(null);
 
   @override
+  Future<Result<void>> dismiss(String sessionId) async => const Result.ok(null);
+
+  @override
   Future<Result<List<NodeRef>>> nodes() async => const Result.ok(<NodeRef>[]);
 
   @override
